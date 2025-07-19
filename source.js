7f967970f0fb70a798c9f33e45b82ff8a47a6266
majd anas <majdanas2004@gmail.com>
2025-07-19 20:11:59 +0300

    testing the commit

--- source.js
+++ source.js
@@ -6,19 +6,34 @@ let input;
     if(input>100)
         input=100;
 
+createGrid(input);
 
 
-const container=document.querySelector(".container");
-for(let i=0;i<input;i++){
-    let column=document.createElement("div");
-    column.setAttribute("class","column")
-    container.appendChild(column);
-    for(let i=0;i<input;i++){
-        let row=document.createElement("div");
-        row.setAttribute("class","row")
-        column.appendChild(row);  
-    }   
+function createGrid(n){
+    const container=document.querySelector(".container");
+    for(let i=0;i<n;i++){
+        let column=document.createElement("div");
+        column.setAttribute("class","column")
+        container.appendChild(column);
+        for(let i=0;i<n;i++){
+            let row=document.createElement("div");
+            row.setAttribute("class","row")
+            column.appendChild(row);  
+        }   
+    }
 }
+
+// const container=document.querySelector(".container");
+// for(let i=0;i<input;i++){
+//     let column=document.createElement("div");
+//     column.setAttribute("class","column")
+//     container.appendChild(column);
+//     for(let i=0;i<input;i++){
+//         let row=document.createElement("div");
+//         row.setAttribute("class","row")
+//         column.appendChild(row);  
+//     }   
+// }
 
 let myRow=document.querySelectorAll(".row");
 
@@ -34,14 +49,14 @@ let myRow=document.querySelectorAll(".row");
 
 
 const reset=document.querySelector("#resetButton");
-reset.addEventListener("click",(button)=>{
-        myRow.forEach((block)=>{
-
-            block.style.backgroundColor="white";
-
-    });
+    reset.addEventListener("click",(button)=>{
+          const contain=document.querySelector("#container");
+          contain.replaceChildren();
+        
 
     input=parseInt(prompt("enter the grid size"));
         if(input>100||input<1)
             input=100;
+    createGrid(input);
+    
 });
