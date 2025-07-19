4f8b63704304d219fd86a9d419242ba46eaaeeb7
majd anas <majdanas2004@gmail.com>
2025-07-19 21:02:17 +0300

    -cleaned up the code with functions -fixed up the ui(did not draw after reset)

--- source.js
+++ source.js
@@ -1,62 +1,54 @@
  
 
-let input;
+const reset=document.querySelector("#resetButton");
+const container=document.querySelector(".container");
 
-    input=parseInt(prompt("enter the grid size"));
-    if(input>100)
-        input=100;
-
-createGrid(input);
-
-
-function createGrid(n){
-    const container=document.querySelector(".container");
-    for(let i=0;i<n;i++){
-        let column=document.createElement("div");
-        column.setAttribute("class","column")
-        container.appendChild(column);
-        for(let i=0;i<n;i++){
-            let row=document.createElement("div");
-            row.setAttribute("class","row")
-            column.appendChild(row);  
-        }   
-    }
+function input(){
+    let n;
+    n=parseInt(prompt("enter the grid size"));
+    if(n>100||n<0)
+        n=100;
+    return n;
 }
 
-// const container=document.querySelector(".container");
-// for(let i=0;i<input;i++){
-//     let column=document.createElement("div");
-//     column.setAttribute("class","column")
-//     container.appendChild(column);
-//     for(let i=0;i<input;i++){
-//         let row=document.createElement("div");
-//         row.setAttribute("class","row")
-//         column.appendChild(row);  
-//     }   
-// }
+function createGrid(n){
+    for(let i=0;i<n;i++){
+    let column=document.createElement("div");
+    column.setAttribute("class","column")
+    container.appendChild(column);
+    for(let i=0;i<n;i++){
+        let row=document.createElement("div");
+        row.setAttribute("class","row")
+        column.appendChild(row);  
+    }   
+}
 
+}
+
+function draw(){
+    
 let myRow=document.querySelectorAll(".row");
-
-    myRow.forEach((block)=>{
-
+        myRow.forEach((block)=>{
             block.addEventListener("mouseover",(e)=>{
             block.style.backgroundColor="black";
-
-
+            
         });
-
     });
 
+}
 
-const reset=document.querySelector("#resetButton");
-    reset.addEventListener("click",(button)=>{
-          const contain=document.querySelector("#container");
-          contain.replaceChildren();
-        
+reset.addEventListener("click",(button)=>{
+    container.replaceChildren();
 
     input=parseInt(prompt("enter the grid size"));
         if(input>100||input<1)
             input=100;
     createGrid(input);
+    draw();
     
 });
+
+
+createGrid(input());
+draw();
+
